fd8112375a4fb000dc4840018f239d1a23dca6db
chouti44 <chouti44@gmail.com>
2018-02-19 09:13:22 +0100

    Add ronan code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,146 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Traderbook</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+    <div class="container-fluid bandeau">
+        <img src="logo.png" id="logo">
+        <div id="title">
+            <h1>Traderbook</h1>
+        </div>
+    </div>
+    <div class="container-fluid bloc" id="bloc1">
+        <div class="row">
+            <div class="col-sm-12 col-lg-4 blocRond">
+                <div class="rond">
+                    <div class="textrond">
+                        Tester
+                    </div>
                 </div>
-            @endif
+            </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="col-lg-3 arrowHorizontal">
+                <span class="arrow"></span>
+            </div>
+            <div class="col-sm-12 col-lg-5 paragraphe">
+                <div class="text">
+                    <h2 class="loremtitle">
+                        Lorem ipsum
+                    </h2>
+                    <p class="loremtext">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci iusto facere cumque, reiciendis dolores illum quos perferendis maiores harum quis dolorem consequatur beatae blanditiis esse aliquid nostrum. Placeat, modi, porro!
+                    </p>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container-fluid bloc" id="bloc2">
+        <div class="row">
+            <div class="col-sm-12 col-lg-4
+			blocRond2">
+                <div class="rond">
+                    <div class="textrond">
+                        Analyser
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 arrowHorizontal">
+                <span class="arrow"></span>
+            </div>
+            <div class="col-sm-12 col-lg-5 paragraphe2">
+                <div class="text">
+                    <h2 class="loremtitle">
+                        Lorem ipsum
+                    </h2>
+                    <p class="loremtext">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci iusto facere cumque, reiciendis dolores illum quos perferendis maiores harum quis dolorem consequatur beatae blanditiis esse aliquid nostrum. Placeat, modi, porro!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bloc" id="bloc3">
+        <div class="row">
+            <div class="col-sm-12 col-lg-4 blocRond">
+                <div class="rond">
+                    <div class="textrond">
+                        Déployer
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 arrowHorizontal">
+                <span class="arrow"></span>
+            </div>
+            <div class="col-sm-12 col-lg-5 paragraphe">
+                <div class="text">
+                    <h2 class="loremtitle">
+                        Lorem ipsum
+                    </h2>
+                    <p class="loremtext">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci iusto facere cumque, reiciendis dolores illum quos perferendis maiores harum quis dolorem consequatur beatae blanditiis esse aliquid nostrum. Placeat, modi, porro!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bloc" id="bloc4">
+        <div class="row">
+            <div class="col-sm-12 col-lg-4 blocRond2">
+                <div class="rond">
+                    <div class="textrond">
+                        Optimiser
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 arrowHorizontal">
+                <span class="arrow"></span>
+            </div>
+            <div class="col-sm-12 col-lg-5 paragraphe">
+                <div class="text">
+                    <h2 class="loremtitle">
+                        Lorem ipsum
+                    </h2>
+                    <p class="loremtext">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci iusto facere cumque, reiciendis dolores illum quos perferendis maiores harum quis dolorem consequatur beatae blanditiis esse aliquid nostrum. Placeat, modi, porro!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bloc" id="bloc5">
+        <div id="newsletter">
+            <p>Inscrivez-vous à la newsletter pour être au courant des nouvautés</p>
+        </div>
+        <input type="text" placeholder="Adresse mail" id="mail">
+        <input type="button" value="Envoyer" id="envoyer">
+    </div>
+    <div class="container-fluid bloc" id="bloc6">
+        <div class="row">
+            <div class="container-fluid" id="reseaux">
+                <div id="reseaux1">
+                    <div id="reseau1" class="reseau">
+                    </div>
+                    <div id="reseau2" class="reseau">
+                    </div>
+                    <div id="reseau3" class="reseau">
+                    </div>
+                    <div id="reseau4" class="reseau">
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid" id="footer">
+                <p>Footer</p>
+            </div>
+        </div>
+    </div>
     </body>
 </html>
